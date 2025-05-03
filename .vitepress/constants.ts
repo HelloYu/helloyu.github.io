184a8522ec391974b201f251af191c7abf824fda
BinMaker <Mr.Yu1991@gmail.com>
2025-05-03 21:09:50 +0800

    chore: change docs path

--- .vitepress/constants.ts
+++ .vitepress/constants.ts
@@ -2,6 +2,7 @@ export const SITE_TITLE = 'Hello Yu'
 export const PAGE_SIZE = 10
 export const POST_FILES = [
 	'blog/**/*.md',
+	'stock/**/*.md',
 	'android/**/*.md',
 	'seo/**/*.md',
 	'raspberrypi/**/*.md',
